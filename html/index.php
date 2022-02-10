dbc85baff6398808e7ebafd8cef0385cc2e2ce66
Yuzunoha <studysteadystandby@gmail.com>
2022-02-10 14:44:36 +0900

    途中まで作った

--- html/index.php
+++ html/index.php
@@ -1,25 +1,58 @@
 <?php
 
-session_start();
+function p($a = '')
+{
+    echo $a . PHP_EOL;
+}
 
-require('controller.php');
+function getPdo()
+{
+    $pdo = new PDO('mysql:host=db;dbname=bbs', 'root', 'root');
+    return $pdo;
+}
 
-if ($_POST) {
-    /* POST Requests */
-    if ($_POST['register_name']) {
-        register($_POST['register_name'], $_POST['register_password']);
-    } else if ($_POST['login_name']) {
-        login($_POST['login_name'], $_POST['login_password']);
-    } else if ($_POST['logout']) {
-        logout();
-    } else if ($_POST['tweet_textarea']) {
-        newtweet($_POST['tweet_textarea']);
-    }
-} else {
-    /* GET Requests */
-    if ($_SESSION['user_id']) {
-        require_once('views/bbs.php');
-    } else {
-        require_once('views/entrance.php');
+function getMessages()
+{
+    $pdo = getPdo();
+    $sql = 'select * from messages';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $ret = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $ret;
+}
+
+function main()
+{
+    p('<pre>');
+    $ret = getMessages();
+    foreach ($ret as $o) {
+        print_r($o);
+        p();
     }
 }
+?>
+
+<html>
+
+<body>
+
+    <table>
+        <?php
+        $ret = getMessages();
+        foreach ($ret as $o) {
+            $id = $o['id'];
+            echo '<tr>';
+            echo '<td>' . $o['text'] . '</td>';
+            echo '<td><button onclick="onclickDelete(' . $id . ')">削除</button></td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
+    <script>
+        const onclickDelete = (id) => {
+            alert('削除～' + id);
+        }
+    </script>
+</body>
+
+</html>
